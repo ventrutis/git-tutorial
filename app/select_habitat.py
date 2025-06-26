65fd0dfa57221a3b30ccf4f1e1ad5f9c7729e5c5
Sara Ventruti <saraventruti@SaraMacBook-Air.local>
2025-06-26 12:00:55 +0200

    implement function

--- app/select_habitat.py
+++ app/select_habitat.py
@@ -19,4 +19,14 @@ def select_habitat(selected_hab_abb=None, habitat_name=None):
         :param habitat_name:
         :param selected_hab_abb:
     """
-    pass
+    url_txt = f"http://opendap.biodt.eu/ias-pdt/0/outputs/hab{selected_hab_abb}/predictions/Prediction_Summary_Shiny.txt"
+    df_mod = pandas.read_csv(url_txt, sep="\t")
+    display(df_mod)
+    habitat_number = str(
+        df_mod[df_mod["hab_name"] == habitat_name]["hab_abb"].values[0])
+
+    filtered_df = df_mod[
+        (df_mod["hab_abb"] == habitat_number)
+        ]
+
+    return filtered_df
